23d4dfe0cf0b7a3cce40edf693c887672ecea95d
Christian Onwe <chrisoftech@gmail.com>
2022-04-10 06:13:36 +0000

    Created & designed home-component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,26 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 const Home = () => {
     return (
         <div className='w-full h-screen bg-[#0a192f]'>
-            <h1>Home</h1>
+            {/* container */}
+            <div className='max-w-[1000px] h-full mx-auto px-8 flex flex-col justify-center'>
+                <p className='text-pink-600'>Hi, my name is</p>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Christian Onwe</h1>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Full Stack & Mobile Application Developer</h2>
+                <p className='text-[#8892b0] py-4 max-w-[700px]'>
+                    I'm a fullstack developer specialized in designing, developing and building exceptional digital experiences.
+                    Currently, am focused on building responsive fullstack, mobile, and blockchain applications.
+                    I have worked with vast amount of technologies professionally and constantly progressing with my set goals and objectives in the knowledge space.
+
+                </p>
+                <div>
+                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+                        View Work
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <HiArrowNarrowRight className='ml-3' />
+                        </span>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
