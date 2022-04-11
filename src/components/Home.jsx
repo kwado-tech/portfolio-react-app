3bde402f5f48f05572fa8f3031bcab7a50613da6
Christian Onwe <chrisoftech@gmail.com>
2022-04-11 20:16:46 +0000

    Created & designed about component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
     return (
-        <div className='w-full h-screen bg-[#0a192f]'>
+        <div name='home' className='w-full h-screen bg-[#0a192f]'>
             {/* container */}
             <div className='max-w-[1000px] h-full mx-auto px-8 flex flex-col justify-center'>
                 <p className='text-pink-600'>Hi, my name is</p>
